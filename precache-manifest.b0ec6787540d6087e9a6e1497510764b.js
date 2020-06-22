24434d6900de4e6ae46c6e4719af4f1eccc00a39
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2020-06-22 21:36:48 +0530

    Updates

---
+++ precache-manifest.b0ec6787540d6087e9a6e1497510764b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093abdde363df1a603fcfabb044e7826",
+    "url": "/Heapsim/index.html"
+  },
+  {
+    "revision": "53635a321e5b3ed55d63",
+    "url": "/Heapsim/static/css/main.05b85a17.chunk.css"
+  },
+  {
+    "revision": "40a9607c8cc0be3a4bf4",
+    "url": "/Heapsim/static/js/2.bddebaa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Heapsim/static/js/2.bddebaa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53635a321e5b3ed55d63",
+    "url": "/Heapsim/static/js/main.8f45f19a.chunk.js"
+  },
+  {
+    "revision": "d2c2f20f0ab6df2eee1d",
+    "url": "/Heapsim/static/js/runtime-main.2c10a0c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Heapsim/static/media/logo.5d5d9eef.svg"
+  }
+]);
